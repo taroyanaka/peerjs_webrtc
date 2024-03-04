210e4da012e153ac9ec25635e82452a9bb077c7d
taroyanaka <6591797+taroyanaka@users.noreply.github.com>
2024-03-04 20:31:05 +0900

    anyway From Mac

--- server.js
+++ server.js
@@ -2,39 +2,23 @@ const cors = require('cors');
 
 
 const express = require('express');
-const { ExpressPeerServer } = require('peer');
-const https = require('https');
-const http = require('http');
+// const https = require('https');
+// const http = require('http');
 
 const app = express();
 app.use(cors());
 
 
-// 実行環境がlocalhostの場合は、httpで実行する、それ以外はhttpsで実行する
-const os = require('os');
 
-console.log(os.hostname());
-
-const server = os.hostname() === 'taninakrounoAir' ? http.createServer(app) : https.createServer(app);
-
-// const port = process.env.PORT || 9000;
-const port = 3000;
+// 実行環境がlocalhostの場合は、httpで実行する
+// const server = http.createServer(app);
+const port = process.env.PORT || 9000;
 // const port = 9000;
+const { ExpressPeerServer } = require('peer');
+const server = app.listen(port, () => {
+  console.log('Server is running on port ' + port);
+});
+const peerServer = ExpressPeerServer(server, {debug: true});
+app.use('/myapp', peerServer);
 
-app.use('/myapp', ExpressPeerServer(server, {
-  debug: true,
-//   path: ''
-}));
-
-
-// server.listen(9000);
-// server.listen(port,'0.0.0.0');
-server.listen(port);
-
-
-
-
-
-// console.log(process.env.PORT);
-
-app.get('/', (req, res, next) => res.send('Hello world! ver0.0.6 port: ' + port.toString() ));
+app.get('/', (req, res, next) => res.send('Hello world! ver0.0.7 port: ' + port.toString() ));
