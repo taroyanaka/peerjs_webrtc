c3b931447044bf035a1eed02684f659ae1d8ce09
taroyanaka <6591797+taroyanaka@users.noreply.github.com>
2024-03-04 20:15:56 +0900

    anyway From Mac

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ const cors = require('cors');
 
 const express = require('express');
 const { ExpressPeerServer } = require('peer');
+const https = require('https');
 const http = require('http');
 
 const app = express();
 app.use(cors());
 
-const server = http.createServer(app);
+
+// 実行環境がlocalhostの場合は、httpで実行する、それ以外はhttpsで実行する
+const os = require('os');
+
+console.log(os.hostname());
+
+const server = os.hostname() === 'taninakrounoAir' ? http.createServer(app) : https.createServer(app);
+
 // const port = process.env.PORT || 9000;
 const port = 3000;
 // const port = 9000;
@@ -29,4 +37,4 @@ server.listen(port);
 
 // console.log(process.env.PORT);
 
-app.get('/', (req, res, next) => res.send('Hello world! port: ' + port.toString() ));
+app.get('/', (req, res, next) => res.send('Hello world! ver0.0.6 port: ' + port.toString() ));
