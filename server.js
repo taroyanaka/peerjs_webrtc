65f5f30acb3efb125fd8c582c5d27c5c32f7ab91
taroyanaka <6591797+taroyanaka@users.noreply.github.com>
2024-03-04 19:49:24 +0900

    anyway From Mac

--- server.js
+++ server.js
@@ -1,44 +1,32 @@
+const cors = require('cors');
+
+
 const express = require('express');
 const { ExpressPeerServer } = require('peer');
 const http = require('http');
 
 const app = express();
+app.use(cors());
+
 const server = http.createServer(app);
-const port = process.env.PORT || 9000;
+// const port = process.env.PORT || 9000;
+const port = 3000;
+// const port = 9000;
 
 app.use('/myapp', ExpressPeerServer(server, {
   debug: true,
   path: '/peerjs'
 }));
-console.log(port);
+
+
 // server.listen(9000);
-server.listen(port,'0.0.0.0');
+// server.listen(port,'0.0.0.0');
+server.listen(port);
 
 
 
 
 
+// console.log(process.env.PORT);
 
-
-// const express = require('express');
-// const { ExpressPeerServer } = require('peer');
-// const app = express();
-// app.get('/', (req, res, next) => res.send('Hello world!'));
-// const server = app.listen(process.env.PORT || 3000);
-// const peerServer = ExpressPeerServer(server, {
-//   path: '/peerjs'
-// });
-// app.use('/peerjs', peerServer);
-// peerServer.on('connection', (client) => { 
-//   console.log('Client connected: ', client.id); 
-// });
-// peerServer.on('disconnect', (client) => { 
-//   console.log('Client disconnected: ', client.id); 
-// });
-
-
-
-
-
-
-app.get('/', (req, res, next) => res.send('Hello world!'));
+app.get('/', (req, res, next) => res.send('Hello world! port: ' + port.toString() ));
