1a3cd22eb6d3012ab97437f2d3ab738d4b8c1a7c
taroyanaka <6591797+taroyanaka@users.noreply.github.com>
2024-04-03 18:58:43 +0900

    anyway From Mac

--- two.sql
+++ two.sql
@@ -3,6 +3,8 @@ DROP TABLE IF EXISTS matches;
 CREATE TABLE matches (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   match_data TEXT NOT NULL,
+  send_id INTEGER NOT NULL,
+  receive_id INTEGER NOT NULL,
   created_at DATETIME NOT NULL,
   updated_at DATETIME NOT NULL
 );
